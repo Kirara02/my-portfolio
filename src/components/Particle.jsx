18294fb1d6cf245f4be284f856f53fcf1d6c8d4d
Kirara02 <fathul.sirnaraja92@gmail.com>
2023-03-25 16:55:22 +0700

    edit particles

--- src/components/Particle.jsx
+++ src/components/Particle.jsx
@@ -22,168 +22,168 @@ function Particle() {
             loaded={particlesLoaded}
             options={
                 {
-                    fps_limit: 120,
-                    background: {                    
-                        position: "50% 50%",
-                        repeat: "no-repeat",
-                        size: "20%",
-                      },
-                      fullScreen: {
-                        zIndex: 1,
-                      },
-                      interactivity: {
-                        events: {
-                          onClick: {
-                            enable: true,
-                            mode: "repulse",
-                          },
-                          onHover: {
-                            enable: true,
-                            mode: "bubble",
-                          },
-                        },
-                        modes: {
-                          bubble: {
-                            distance: 250,
-                            duration: 2,
-                            opacity: 0,
-                            size: 0,
-                          },
-                          grab: {
-                            distance: 400,
-                          },
-                          repulse: {
-                            distance: 400,
-                          },
-                        },
-                      },
-                      particles: {
-                        color: {
-                          value: "#ffffff",
-                        },
-                        links: {
-                          color: {
-                            value: "#ffffff",
-                          },
-                          distance: 150,
-                          opacity: 0.2,
-                        },
-                        move: {
-                          attract: {
-                            rotate: {
-                              x: 800,
-                              y: 800,
-                            },
-                          },
-                          enable: true,
-                          outModes: {
-                            bottom: "out",
-                            left: "out",
-                            right: "out",
-                            top: "out",
-                          },
-                          random: true,
-                          speed: 1,
-                        },
-                        number: {
-                          density: {
-                            enable: true,
-                          },
-                          value: 160,
-                        },
-                        opacity: {
-                          random: {
-                            enable: true,
-                          },
-                          value: {
-                            min: 0,
-                            max: 1,
-                          },
-                          animation: {
-                            enable: true,
-                            speed: 3,
-                            minimumValue: 0,
-                          },
-                        },
-                        size: {
-                          random: {
-                            enable: true,
-                          },
-                          value: {
-                            min: 1,
-                            max: 3,
-                          },
-                          animation: {
-                            speed: 4,
-                            minimumValue: 0.3,
-                          },
-                        },
-                      },
-                    // fpsLimit: 120,
-                    // interactivity: {
+                    // fps_limit: 120,
+                    // background: {                    
+                    //     position: "50% 50%",
+                    //     repeat: "no-repeat",
+                    //     size: "20%",
+                    //   },
+                    //   fullScreen: {
+                    //     zIndex: 1,
+                    //   },
+                    //   interactivity: {
                     //     events: {
-                    //         onClick: {
-                    //             enable: true,
-                    //             mode: "push",
-                    //         },
-                    //         onHover: {
-                    //             enable: true,
-                    //             mode: "repulse",
-                    //         },
-                    //         resize: true,
+                    //       onClick: {
+                    //         enable: true,
+                    //         mode: "repulse",
+                    //       },
+                    //       onHover: {
+                    //         enable: true,
+                    //         mode: "bubble",
+                    //       },
                     //     },
                     //     modes: {
-                    //         push: {
-                    //             quantity: 4,
-                    //         },
-                    //         repulse: {
-                    //             distance: 150,
-                    //             duration: 0.4,
-                    //         },
+                    //       bubble: {
+                    //         distance: 250,
+                    //         duration: 2,
+                    //         opacity: 0,
+                    //         size: 0,
+                    //       },
+                    //       grab: {
+                    //         distance: 400,
+                    //       },
+                    //       repulse: {
+                    //         distance: 400,
+                    //       },
                     //     },
-                    // },
-                    // particles: {
+                    //   },
+                    //   particles: {
                     //     color: {
-                    //         value: "#ffffff",
+                    //       value: "#ffffff",
                     //     },
                     //     links: {
-                    //         color: "#ffffff",
-                    //         distance: 150,
-                    //         enable: true,
-                    //         opacity: 0.5,
-                    //         width: 1,
-                    //     },
-                    //     collisions: {
-                    //         enable: true,
+                    //       color: {
+                    //         value: "#ffffff",
+                    //       },
+                    //       distance: 150,
+                    //       opacity: 0.2,
                     //     },
                     //     move: {
-                    //         direction: "none",
-                    //         enable: true,
-                    //         outModes: {
-                    //             default: "bounce",
+                    //       attract: {
+                    //         rotate: {
+                    //           x: 800,
+                    //           y: 800,
                     //         },
-                    //         random: false,
-                    //         speed: 3,
-                    //         straight: false,
+                    //       },
+                    //       enable: true,
+                    //       outModes: {
+                    //         bottom: "out",
+                    //         left: "out",
+                    //         right: "out",
+                    //         top: "out",
+                    //       },
+                    //       random: true,
+                    //       speed: 1,
                     //     },
                     //     number: {
-                    //         density: {
-                    //             enable: true,
-                    //             area: 800,
-                    //         },
-                    //         value: 80,
+                    //       density: {
+                    //         enable: true,
+                    //       },
+                    //       value: 160,
                     //     },
                     //     opacity: {
-                    //         value: 0.5,
-                    //     },
-                    //     shape: {
-                    //         type: "circle",
+                    //       random: {
+                    //         enable: true,
+                    //       },
+                    //       value: {
+                    //         min: 0,
+                    //         max: 1,
+                    //       },
+                    //       animation: {
+                    //         enable: true,
+                    //         speed: 3,
+                    //         minimumValue: 0,
+                    //       },
                     //     },
                     //     size: {
-                    //         value: { min: 1, max: 5 },
+                    //       random: {
+                    //         enable: true,
+                    //       },
+                    //       value: {
+                    //         min: 1,
+                    //         max: 3,
+                    //       },
+                    //       animation: {
+                    //         speed: 4,
+                    //         minimumValue: 0.3,
+                    //       },
                     //     },
-                    // },
-                    // detectRetina: true,
+                    //   },
+                    fpsLimit: 120,
+                    interactivity: {
+                        events: {
+                            onClick: {
+                                enable: true,
+                                mode: "push",
+                            },
+                            onHover: {
+                                enable: true,
+                                mode: "repulse",
+                            },
+                            resize: true,
+                        },
+                        modes: {
+                            push: {
+                                quantity: 4,
+                            },
+                            repulse: {
+                                distance: 150,
+                                duration: 0.4,
+                            },
+                        },
+                    },
+                    particles: {
+                        color: {
+                            value: "#ffffff",
+                        },
+                        links: {
+                            color: "#ffffff",
+                            distance: 150,
+                            enable: true,
+                            opacity: 0.5,
+                            width: 1,
+                        },
+                        collisions: {
+                            enable: true,
+                        },
+                        move: {
+                            direction: "none",
+                            enable: true,
+                            outModes: {
+                                default: "bounce",
+                            },
+                            random: false,
+                            speed: 2,
+                            straight: false,
+                        },
+                        number: {
+                            density: {
+                                enable: true,
+                                area: 800,
+                            },
+                            value: 80,
+                        },
+                        opacity: {
+                            value: 0.5,
+                        },
+                        shape: {
+                            type: "circle",
+                        },
+                        size: {
+                            value: { min: 1, max: 5 },
+                        },
+                    },
+                    detectRetina: true,
                 }
             }
         />
